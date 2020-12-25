fa5738d16bea7c5e0c5f865bb456c8c3ac2dcd48
Kirill <kirill07.07.01@mail.ru>
2020-12-25 20:41:12 +0300

    Доработка

--- dateRequire.js
+++ dateRequire.js
@@ -1,10 +1,17 @@
 const os = require('os')
 const greeting = require('./greeting')
 let username = os.userInfo().username;
-
-module.exports.time = username + `\nДата запроса ${greeting.date}` + "\n" + greeting.getMessage(username)
+module.exports = {
+    write: ()=>{ 
+                console.log(username)
+                console.log(`Дата запроса ${greeting.date}`)
+                console.log(greeting.getMessage(username))
+    }
+}
     
     //console.log(username)
     //console.log(`Дата запроса ${greeting.date}`)
     //console.log(greeting.getMessage(username))
     //return username + `\nДата запроса ${greeting.date}` + "\n" + greeting.getMessage(username)
+
+
