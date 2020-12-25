6bc0d38159c37ea9276899190a420c675a8af042
Kirill <kirill07.07.01@mail.ru>
2020-12-25 18:41:22 +0300

    install OS

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 const express = require('express')
+const os = require('os')
+
+const app = express()
+const port = process.env.port || 3000
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res)=>{
+    console.log(os.userInfo().username)
+})
+
+app.listen(port, ()=>{
+    console.log(`port ${port}`)
+})
