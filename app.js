c513d664657541708b46d97f7f7b3282fa78c55d
Kirill <kirill07.07.01@mail.ru>
2020-12-25 19:05:52 +0300

    Время запроса пользователя

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const os = require('os')
+const greeting = require('./greeting')
 
 const app = express()
 const port = process.env.port || 3000
@@ -7,7 +8,10 @@ const port = process.env.port || 3000
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res)=>{
-    console.log(os.userInfo().username)
+    let username = os.userInfo().username;
+    console.log(username)
+    console.log(`Дата запроса ${greeting.date}`)
+    console.log(greeting.getMessage(username))
 })
 
 app.listen(port, ()=>{
