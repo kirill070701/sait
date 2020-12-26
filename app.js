f0fee628586f664ce02664211e7181fc36a6ed24
Kirill <kirill07.07.01@mail.ru>
2020-12-26 14:32:42 +0300

    Установка fs(file-system)

--- app.js
+++ app.js
@@ -12,26 +12,26 @@ app.get('/', (req, res)=>{
     res.end(dateRequire.writeDate)
     console.log("--------------")
 
-    function display(data, callback){
- 
-        // с помощью случайного числа определяем ошибку
-        var randInt = Math.random() * (10 - 1) + 1;
-        var err = randInt>5? new Error("Ошибка выполнения. randInt больше 5"): null;
-         
-        setTimeout(function(){
-            callback(err, data);
-        }, 0);
+    function displaySync(callback){
+        callback();
     }
      
     console.log("Начало работы программы");
      
-    display("Обработка данных...", function (err, data){
+    setTimeout(function(){
+             
+            console.log("timeout 500");
+    }, 500);
      
-        if(err) throw err;
-        console.log(data);
-    });
+    setTimeout(function(){
+             
+            console.log("timeout 100");
+    }, 100);
      
+    displaySync(function(){console.log("without timeout")});
     console.log("Завершение работы программы");
+
+        
 
 })
 
